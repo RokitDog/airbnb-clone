335a483131558ffbcd83e97aa12da77a99468d0d
Rokit <bojanforgaming@hotmail.com>
2021-08-11 15:25:21 +0200

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+function Footer() {
+    return (
+        <footer className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100  text-gray-600'>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'>ABOUT</h5>
+                <p>How Airbnb Works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'>COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>It's a pretty awesome clone</p>
+                <p>Referrals accepter</p>
+                <p>Rokit Dog</p>
+
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'>HOST</h5>
+                <p>Let's Go</p>
+                <p>Presents</p>
+                <p>Zero to Full Stack Hero</p>
+                <p>Hundreds of Sites</p>
+                <p>Join Now</p>
+
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'>SUPPORT</h5>
+                <p>Help Center</p>
+                <p>Trust & Safety</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer
